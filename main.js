fc9cfe721a46d308b649eaf50c7a33a7b6c81a7e
JakeTurnick <jaketurnick@gmail.com>
2023-01-25 01:11:28 -0500

    idk I don't have a handle on handlebars

--- main.js
+++ main.js
@@ -1,6 +1,23 @@
 // from data I need: {
+//     images.url(sizeXsize)
 //     title:
 //     price:
-//     images.url(sizeXsize)
+//     author:
+//     more-link:
 // }
 
+import { rawData } from "./data.js";
+
+(function () {
+  "use strict";
+
+  const source = document.getElementById("product-template").innerHTML;
+  const template = Handlebars.compile(source);
+
+  const context = {
+    rawData,
+  };
+  const html = template(context);
+
+  document.getElementById("product").insertAdjacentHTML("afterbegin", html);
+})();
